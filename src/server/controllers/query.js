7465196c7541127ccabb2ce2c853eabdffadaea8
benghui <benghui@gmail.com>
2018-11-18 16:31:01 +0800

    modified routes to connect db

--- src/server/controllers/query.js
+++ src/server/controllers/query.js
@@ -1,16 +1,13 @@
 const request = require('request');
 
-module.exports = {
-  get: (req, res) => {
-    const query = req.query.search;
+module.exports = (db) => {
+  (req, res) => {
 
-    const url = 'test';
-
-    request(url, (error, queryResponse, body) => {
+    request((error, queryResponse, body) => {
       console.log('error:', error); // Print the error if one occurred and handle it
       console.log('statusCode:', queryResponse && queryResponse.statusCode); // Print the response status code if a response was received
 
       res.send(body);
     });
   },
-};
+}
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,5 +1,3 @@
 module.exports = (app, db) => {
-  const query = require('./controllers/query')(db);
-
-  app.get('/api/query', query.get);
+  app.get('/api', app.get);
 };
